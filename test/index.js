b6f2045161acf104768211a2144f92a023e73f64
quilicicf <cquilici@restlet.com>
2016-01-31 14:27:20 +0100

    :white_check_mark: Refactor tests

---
+++ test/index.js
@@ -0,0 +1,41 @@
+var should = require('chai').should(),
+    ds = require('../index'),
+    parser = require('../index.js'),
+    fs = require('fs'),
+    fr = require('../lib/folderReader.js'),
+    _ = require('lodash-node'),
+    assert = require('assert');
+
+describe('jsonPointer2lineNumber' , function () {
+  var resourcesDir = __dirname + '/resources';
+  var jsonDir = resourcesDir + '/json';
+  var expectationsDir = resourcesDir + '/expectations';
+
+  var jsonFiles = fr._getFilesFromFolder(jsonDir);
+  var expectationsFiles = fr._getFilesFromFolder(expectationsDir);
+
+  if (!_.isEqual(jsonFiles, expectationsFiles)) {
+    var missingExpectationsFiles = _.difference(jsonFiles, expectationsFiles);
+    if (!_.isEmpty(missingExpectationsFiles)) {
+      throw new Error('Some JSON files do not have their expectations: ' + JSON.stringify(missingExpectationsFiles));
+    }
+
+    var missingJsonFiles = _.difference(expectationsFiles, jsonFiles);
+    if (!_.isEmpty(missingJsonFiles)) {
+      throw new Error('Some expectations files do not have their JSON: ' + JSON.stringify(missingJsonFiles));
+    }
+  }
+
+  _.each(jsonFiles, function (file) {
+    describe('file ' + file, function() {
+      var json = fs.readFileSync(jsonDir + '/' + file, 'utf8');
+      var expectations = JSON.parse(fs.readFileSync(expectationsDir + '/' + file, 'utf8'));
+
+      _.each(expectations, function (expectation) {
+        it('should return ' + expectation.line + ' for JSON pointer ' + expectation.jsonPointer, function() {
+          assert.equal(expectation.line, parser.getLineNumber(json, expectation.jsonPointer));
+        });
+      });
+    });
+  });
+});
